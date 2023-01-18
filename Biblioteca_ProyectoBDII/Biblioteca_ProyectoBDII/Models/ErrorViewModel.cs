982f86253b29acfc4fca29cf3fc329c45465bb2c
Bryan-Vallejos <josuevallejos16@gmail.com>
2023-01-18 09:36:56 -0600

    Primer commit

---
+++ Biblioteca_ProyectoBDII/Biblioteca_ProyectoBDII/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Biblioteca_ProyectoBDII.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
